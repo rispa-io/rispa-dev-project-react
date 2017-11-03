9cb7de79516e1a65b196abfef3ea48047d9e58cb
dvpetrov <dvpetrov.main@gmail.com>
2017-11-03 15:37:26 +0300

    Remove old config && update package.json

--- packages/config/activator.js
+++
@@ -1,6 +0,0 @@
-const { createConfigActivator, default: api } = require('@rispa/config')
-const config = require('./config')
-
-module.exports.default = createConfigActivator(config)
-
-module.exports.api = api
